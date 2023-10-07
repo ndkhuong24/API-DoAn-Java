d8ba214986e23b47ad7e6e689e8151b317abda64
quynhdt <quynhdt@gct.com.vn>
2023-10-07 12:46:42 +0700

    sửa

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -45,39 +45,17 @@ BCryptPasswordEncoder pe;
     /* Phân quyền */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/order/**").authenticated()
-                .antMatchers("/admin/**").hasAnyRole("STAF", "DIRE")
-                .antMatchers("/rest/authorities").hasRole("DIRE")
-                .anyRequest().permitAll();
-
-        http.formLogin()
-                .loginPage("/security/login/form")  /* địa chỉ form đăng nhập */
-                .loginProcessingUrl("/security/login") /* địa chỉ khi nhất đăng nhập */
-                .defaultSuccessUrl("/security/login/success", false) /* địa chỉ xử lý đăng nhập khi thành công - False: Đăng nhập thành công không nhất thiết quay về trang success*/
-                .failureUrl("/security/login/error"); /* địa chỉ xử lý đăng nhập khi lỗi */
-
-        http.rememberMe()
-                .tokenValiditySeconds(86400); /* xác định thời gian tồn tại cookies (thời gian tự động login có hiệu lực) */
-
-        http.exceptionHandling()
-                .accessDeniedPage("/security/unauthoried"); /* địa chỉ khi đăng nhập đường dẫn chưa cấp quyền - VD: CUSTOMER đăng nhập đường dẫn của DIRE*/
-
-        http.logout()
-                .logoutUrl("/security/logoff") /* địa chỉ đường dẫn khi đăng xuất */
-                .logoutSuccessUrl("/security/logoff/success"); /* địa chỉ xử lý dăng xuất khi thành công */
-    }
-
-    /* Cơ chế mã hóa mật khẩu */
-    @Bean
-    public BCryptPasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    /* Cho phép truy xuất REST API từ bên ngoài (domain khác ) */
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+        http
+                .authorizeRequests()
+                .antMatchers("/api/Customer").permitAll() // Cho phép tất cả mọi người truy cập vào địa chỉ này
+                .antMatchers("/api/User/**").hasRole("ADMIN") // Yêu cầu có quyền ADMIN để truy cập các địa chỉ này
+                .anyRequest().authenticated() // Tất cả các request khác đều cần phải xác thực mới được truy cập
+                .and()
+                .formLogin() // Cho phép người dùng xác thực bằng form login
+                .defaultSuccessUrl("/api/User")
+                .permitAll() // Tất cả đều được truy cập vào địa chỉ này
+                .and()
+                .logout() // Cho phép logout
+                .permitAll();
     }
 }
