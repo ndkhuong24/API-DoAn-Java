5c1e6e354fd23de7b0d89e9e7044d7e7d9358bb9
DESKTOP-O564RM8\alfon <alfonsominh0512@gmail.com>
2023-11-16 15:41:49 +0700

    api search

--- src/main/java/com/example/demo/controller/FilterController.java
+++ src/main/java/com/example/demo/controller/FilterController.java
@@ -4,39 +4,44 @@ import com.example.demo.model.Filter;
 import com.example.demo.model.ProductDetail;
 import com.example.demo.repository.IFilterRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/filter")
+@RequestMapping("/api")
 @CrossOrigin("*")
 public class FilterController {
     @Autowired
     private IFilterRespository repos;
 
-
-    @GetMapping
+    @GetMapping("/filter")
     public List<Filter> filterProduct(@RequestParam(required = false) Double minPrice,
-                                             @RequestParam(required = false) Double maxPrice,
-                                             @RequestParam(required = false) List<String> colors,
-                                             @RequestParam(required = false)List<String>brands,
-                                            @RequestParam(required = false)List<String>sizes,
-                                            @RequestParam(required = false)List<String>soles,
-                                            @RequestParam(required = false)List<String>materials) {
+                                      @RequestParam(required = false) Double maxPrice,
+                                      @RequestParam(required = false) List<String> colors,
+                                      @RequestParam(required = false) List<String> brands,
+                                      @RequestParam(required = false) List<String> sizes,
+                                      @RequestParam(required = false) List<String> soles,
+                                      @RequestParam(required = false) List<String> materials) {
         List<Filter> list = repos.filter();
 
         return list.stream()
                 .filter(product -> (minPrice == null || product.getPrice() >= minPrice) &&
-                        (maxPrice == null || product.getPrice() <= maxPrice)&&
-                        (colors == null || colors.isEmpty() || colors.contains(product.getColorName()))&&
-                        (brands==null || brands.isEmpty()||brands.contains(product.getBrandName()))&&
-                        (sizes==null || sizes.isEmpty()||sizes.contains(product.getSizeName()))&&
-                        (soles==null || soles.isEmpty()||soles.contains(product.getSoleName()))&&
-                        (materials==null || materials.isEmpty()||materials.contains(product.getMaterialName()))
+                        (maxPrice == null || product.getPrice() <= maxPrice) &&
+                        (colors == null || colors.isEmpty() || colors.contains(product.getColorName())) &&
+                        (brands == null || brands.isEmpty() || brands.contains(product.getBrandName())) &&
+                        (sizes == null || sizes.isEmpty() || sizes.contains(product.getSizeName())) &&
+                        (soles == null || soles.isEmpty() || soles.contains(product.getSoleName())) &&
+                        (materials == null || materials.isEmpty() || materials.contains(product.getMaterialName()))
                 )
                 .collect(Collectors.toList());
 
+    }
+
+    @GetMapping("/search/{name}")
+    public ResponseEntity<List> search(@PathVariable String name ){
+        return ResponseEntity.ok(repos.search(name));
     }
 }
--- src/main/java/com/example/demo/repository/IFilterRespository.java
+++ src/main/java/com/example/demo/repository/IFilterRespository.java
@@ -23,6 +23,20 @@ public interface IFilterRespository extends JpaRepository<Filter,Integer> {
             "\t\tJOIN Material AS M ON M.id = PD.material_id\n" +
             "\t\tJOIN ImageChinh AS IC ON PD.id = IC.product_detail_id", nativeQuery = true)
     List<Filter> filter();
+
+    @Query(value = "SELECT PD.id AS id, IC.path AS path, P.name AS product_name, C.name AS category_name, B.name AS brand_name, S.name AS size_name, Cl.name AS color_name, Sol.name AS sole_name, M.name AS material_name,PD.price AS price\n" +
+            "            FROM \n" +
+            "            ProductDetail AS PD\n" +
+            "            JOIN Product AS P ON PD.product_id = P.id \n" +
+            "            JOIN Category AS C ON C.id = PD.category_id\n" +
+            "            JOIN Brand AS B ON B.id = PD.brand_id\n" +
+            "            JOIN Size AS S ON S.id = PD.size_id\n" +
+            "            JOIN Color AS Cl ON Cl.id = PD.color_id\n" +
+            "            JOIN Sole AS Sol ON Sol.id = PD.sole_id\n" +
+            "            JOIN Material AS M ON M.id = PD.material_id\n" +
+            "            JOIN ImageChinh AS IC ON PD.id = IC.product_detail_id where P.name Like %?1%",nativeQuery = true)
+    List<Filter> search(String name);
 }
+
 
 
