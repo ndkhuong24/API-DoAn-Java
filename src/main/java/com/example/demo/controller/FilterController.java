be9b02991915e60081d4993fa326853d2b7c7dee
DESKTOP-O564RM8\alfon <alfonsominh0512@gmail.com>
2023-11-22 10:17:01 +0700

    update filter

--- src/main/java/com/example/demo/controller/FilterController.java
+++ src/main/java/com/example/demo/controller/FilterController.java
@@ -23,7 +23,8 @@ public class FilterController {
                                       @RequestParam(required = false) List<String> brands,
                                       @RequestParam(required = false) List<String> sizes,
                                       @RequestParam(required = false) List<String> soles,
-                                      @RequestParam(required = false) List<String> materials) {
+                                      @RequestParam(required = false) List<String> materials,
+                                      @RequestParam(required = false) List<String>products) {
         List<Filter> list = repos.filter();
 
         return list.stream()
@@ -33,12 +34,21 @@ public class FilterController {
                         (brands == null || brands.isEmpty() || brands.contains(product.getBrandName())) &&
                         (sizes == null || sizes.isEmpty() || sizes.contains(product.getSizeName())) &&
                         (soles == null || soles.isEmpty() || soles.contains(product.getSoleName())) &&
-                        (materials == null || materials.isEmpty() || materials.contains(product.getMaterialName()))
+                        (materials == null || materials.isEmpty() || materials.contains(product.getMaterialName()))&&
+                        (products==null||products.isEmpty()||isSimilarProductPresent(products, product.getProductName()))
                 )
                 .collect(Collectors.toList());
 
     }
-
+    private boolean isSimilarProductPresent(List<String> products, String productName) {
+        for (String product : products) {
+            // Sử dụng phương thức contains để kiểm tra xem chuỗi brand có tồn tại trong brandName hay không
+            if (productName.toLowerCase().contains(product.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
     @GetMapping("/search/{name}")
     public ResponseEntity<List> search(@PathVariable String name ){
         return ResponseEntity.ok(repos.search(name));
